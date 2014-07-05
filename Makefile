73145c98896fd00a8566c554766b9f00423f2b17
Ben Longbons <b.r.longbons@gmail.com>
2014-07-05 09:06:33 -0700

    Port new tap libs

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 RUSTC = rustc
 RUSTCFLAGS = -g --opt-level=2
 
-PKGCONFIG_TERMKEY = $$(pkg-config --libs tickit) $$(pkg-config --libs-only-L tickit | sed 's:-L:-Wl,-rpath=:')
+PKGCONFIG_TICKIT = $$(pkg-config --libs tickit) $$(pkg-config --libs-only-L tickit | sed 's:-L:-Wl,-rpath=:')
 
 default: tickit
 all: demo tickit
@@ -13,7 +13,7 @@ source.stamp: $(filter-out src/test.rs,$(wildcard src/*.rs))
 	@touch $@
 
 tickit.stamp: src/lib.rs source.stamp
-	${RUSTC} ${RUSTCFLAGS} $< -C link-args="${PKGCONFIG_TERMKEY}"
+	${RUSTC} ${RUSTCFLAGS} $< -C link-args="${PKGCONFIG_TICKIT}"
 	@touch $@
 tickit: tickit.stamp
 .SUFFIXES:
--- src/test.rs
+++ src/test.rs
@@ -1,6 +1,158 @@
+#![feature(macro_rules)]
+
+extern crate libc;
+
 extern crate tickit;
 
-#[test]
-fn test_nothing()
+macro_rules! diag(
+    ($($arg:tt)*) => ({
+        let dst: &mut ::std::io::Writer = &mut ::std::io::stderr();
+        let _ = write!(dst, "# ");
+        let _ = writeln!(dst, $($arg)*);
+    })
+)
+
+mod taplib
 {
+    pub struct Tap
+    {
+        nexttest: uint,
+        total: uint,
+        _fail: bool,
+    }
+
+    impl Tap
+    {
+        pub fn new() -> Tap
+        {
+            Tap{nexttest: 1, total: 0, _fail: false}
+        }
+    }
+
+    impl Drop for Tap
+    {
+        fn drop(&mut self)
+        {
+            if self.total == 0
+            {
+                let t = self.nexttest - 1;
+                self.plan_tests(t)
+            }
+
+            if self.total != self.nexttest - 1
+            {
+                diag!("Expected {} tests, got {}", self.total, self.nexttest - 1);
+                self._fail = true;
+            }
+            if self._fail
+            {
+                if !::std::task::failing()
+                {
+                    fail!()
+                }
+                else
+                {
+                    diag!("avoiding double-fail!() ...")
+                }
+            }
+        }
+    }
+
+    impl Tap
+    {
+        pub fn plan_tests(&mut self, n: uint)
+        {
+            self.total = n;
+            println!("1..{}", n);
+        }
+        #[allow(dead_code)]
+        pub fn finish(&mut self)
+        {
+            self.total = self.nexttest - 1;
+        }
+
+        pub fn pass(&mut self, name: &str)
+        {
+            println!("ok {} - {}", self.nexttest, name);
+            self.nexttest += 1;
+        }
+
+        pub fn fail(&mut self, name: &str)
+        {
+            println!("not ok {} - {}", self.nexttest, name);
+            self.nexttest += 1;
+            self._fail = true;
+        }
+
+        pub fn ok(&mut self, cmp: bool, name: &str)
+        {
+            if cmp
+            {
+                self.pass(name);
+            }
+            else
+            {
+                self.fail(name);
+            }
+        }
+
+        pub fn bypass(&mut self, count: uint, name: &str)
+        {
+            self.fail(name);
+            self.nexttest -= 1;
+            self.nexttest += count;
+        }
+
+        pub fn is_int<T: PartialEq + ::std::fmt::Show>(&mut self, got: T, expect: T, name: &str)
+        {
+            if got == expect
+            {
+                self.ok(true, name);
+            }
+            else
+            {
+                self.ok(false, name);
+                diag!("got {} expected {} in: {}", got, expect, name);
+            }
+        }
+        pub fn is_str<T: Str, U: Str>(&mut self, got: T, expect: U, name: &str)
+        {
+            let got = got.as_slice();
+            let expect = expect.as_slice();
+
+            if got == expect
+            {
+                self.ok(true, name);
+            }
+            else
+            {
+                self.ok(false, name);
+                // differs from generic version by the ''s.
+                diag!("got '{}' expected '{}' in: {}", got, expect, name);
+            }
+        }
+
+        pub fn is_str_escape<T: Str, U: Str>(&mut self, got: T, expect: U, name: &str)
+        {
+            let got = got.as_slice();
+            let expect = expect.as_slice();
+
+            if got == expect
+            {
+                self.ok(true, name);
+            }
+            else
+            {
+                self.ok(false, name);
+                diag!("got \"{}\" expected \"{}\" in: {}", got.escape_default(), expect.escape_default(), name);
+            }
+        }
+    }
+}
+
+#[test]
+fn test_00nothing()
+{
+    let mut tap = taplib::Tap::new();
+    tap.is_str_escape("'\n\"", "\"\\n'", "nothing");
 }
